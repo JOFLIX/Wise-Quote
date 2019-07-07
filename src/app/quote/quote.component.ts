010e9a2246ddd6d33051a69af28048b5b3705035
JOFLIX <joflixooko@outlook.com>
2019-07-07 23:51:50 +0300

    last commit

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import{Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
     quotes:Quote[] = [
-        new Quote(1,'Joflix(Dr.Prof.Eng.CGHE)','Success is determined first by the belief system then hardwork and determination',new Date(2018,3,14)),
-        new Quote(2,'Joflix','Respect is not give, respect is earned!',new Date(2018,6,9)),
-        new Quote(3, 'Joflix(Dr.Prof.Eng.CGHE)','True sapience is when one falls in love, but do not get into confluence, but even truest, is when one do not fall in love!',new Date(2018,1,12)),
-        new Quote(4, 'Joflix(Dr.Prof.Eng.CGHE)','Mistakes are good, you should make more!',new Date(2018,0,18)),
-        new Quote(5, 'Joflix(Dr.Prof.Eng.CGHE)','Most of the time we fear to fail, forgetting that we are not judged by how much we fail but by how much stronger we come out of the failure!',new Date(2018,2,14) ),
-        new Quote(6, 'Joflix(Dr.Prof.Eng.CGHE)','We only have one true friend, a universal friend, the our shadow!',new Date(2019,7,14)),
+        new Quote(1,'Joflix(Dr.Prof.Eng.CGHE)',"",'Success is determined first by the belief system then hardwork and determination',new Date(2018,3,14)),
+        new Quote(2,'Joflix',"",'Respect is not give, respect is earned!',new Date(2018,6,9)),
+        new Quote(3, 'Joflix(Dr.Prof.Eng.CGHE)',"",'True sapience is when one falls in love, but do not get into confluence, but even truest, is when one do not fall in love!',new Date(2018,1,12)),
+        new Quote(4, 'Joflix(Dr.Prof.Eng.CGHE)',"",'Mistakes are good, you should make more!',new Date(2018,0,18)),
+        new Quote(5, 'Joflix(Dr.Prof.Eng.CGHE)',"",'Most of the time we fear to fail, forgetting that we are not judged by how much we fail but by how much stronger we come out of the failure!',new Date(2018,2,14) ),
+        new Quote(6, 'Joflix(Dr.Prof.Eng.CGHE)',"",'We only have one true friend, a universal friend, the our shadow!',new Date(2019,7,14)),
 
     ];
     addNewQuote(quote){
